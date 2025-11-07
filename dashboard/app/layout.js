5db867cc1a0b8bae10348f46b5197a93049c50ee
Milton Bolonha <105504675+milton-bolonha@users.noreply.github.com>
2025-11-07 17:01:20 -0300

    Add keys to ClerkProvider in RootLayout

--- dashboard/app/layout.js
+++ dashboard/app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      secretKey={process.env.CLERK_SECRET_KEY}
+    >
       <html lang="pt" className="h-full" suppressHydrationWarning>
         <body className="h-full">
           <ThemeProvider
